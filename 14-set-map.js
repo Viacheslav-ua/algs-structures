5e7ed59a7f37c8a573dbb2faf92184b6c347daf7
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2022-10-16 09:50:34 +0300

    finish

---
+++ 14-set-map.js
@@ -0,0 +1,19 @@
+const map = new Map()
+
+const objKey = {id: 5}
+
+map.set(objKey, 'alg tv')
+
+console.log(map.get(objKey))
+
+const set = new Set()
+
+set.add(2)
+set.add(5)
+set.add(2)
+set.add(2)
+set.add(5)
+set.add(7)
+set.add(8)
+
+console.log(set)
